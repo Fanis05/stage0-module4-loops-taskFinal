b22017dbf4714bc4a5bffefcfae073702b27d3d1
Fanis <mrduck597@gmail.com>
2023-05-13 23:08:53 +0500

    test1

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,16 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int y =1; y<=cathetusLength; y++){
+            for (int x = 1; x<=cathetusLength; x++){
+                if (x<=cathetusLength - y) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for(int y = 1; y<=cathetusLength; y++){
+            for (int x = cathetusLength; x>y; x--){
+                System.out.print(" ");
+            }
+            for (int x=y; x>1; x--){
+                System.out.print(x);
+            }
+            for (int x=1; x<=y; x++){
+                System.out.print(x);
+            }
+            System.out.println();
+        }
 
     }
 
